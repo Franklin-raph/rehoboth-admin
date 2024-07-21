37a62d0065718660ac9e1f1e8bdc4ad219842680
franklin-raph <igboekwulusifranklin@gmail.com>
2024-07-21 22:13:46 +0100

    update

--- src/components/delete-task-modal/DeleteTaskModal.jsx
+++ src/components/delete-task-modal/DeleteTaskModal.jsx
@@ -1,6 +1,37 @@
 import React from 'react'
+import Cookies from 'js-cookie'
 
 const DeleteTaskModal = ({taskId, getTasks, setModal}) => {
+
+  const user = Cookies.get('token')
+  const BASE_URL = import.meta.env.VITE_BASE_URL
+
+  async function deleteTask(){
+    // setLoading(true)
+    try {
+      const response = await fetch(`${BASE_URL}/admin/referralProgram/delete-task/${taskId}`, {
+        method: 'DELETE',
+        headers: {
+          'Api-Key': `GISUYDre8wt7984yupor5jp80YT%^%Rfuyih2wrk*&*^%&$^HJLIUTYDFwe576284`,
+          'Authorization': `Bearer ${user}`,
+        }
+      })
+
+      if(!response.ok){
+        throw new Error('Failed to delete task')
+      }
+
+      await response.json()
+      setModal(false)
+      getTasks()
+      // setLoading(false)
+
+    } catch (error) {
+      console.error(error)
+      // setLoading(false)
+    }
+  }
+
   return (
     <>
       <div className="h-full w-full fixed top-0 left-0 z-[101]" style={{ background:"rgba(14, 14, 14, 0.58)" }} onClick={() => setModal(false)}></div>
@@ -13,7 +44,7 @@ const DeleteTaskModal = ({taskId, getTasks, setModal}) => {
             <p className='text-[20px] text-gray-500 font-bold my-10'>Are you sure you want to delete this task?</p>
             <div className='flex gap-5'>
               <button className='text-gray-500 border border-primary-color px-4 py-1 rounded-sm' onClick={() => setModal(false)}>No</button>
-              <button className='bg-primary-color text-white px-4 py-1 rounded-sm'>Yes, Continue</button>
+              <button className='bg-primary-color text-white px-4 py-1 rounded-sm' onClick={deleteTask}>Yes, Continue</button>
             </div>
           </div>
         </div>
